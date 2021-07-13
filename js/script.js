a7c612f5b61cbe5a5b1ad788379a594bad5271be
Kostas <kostas.sliazas@gmail.com>
2021-07-13 04:52:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@
   }
   const matchSimbols = /(?:[</]{2,2}|<|>|=)/g
   const matchTexts = /\b(?<=\s)src|href|content|defer|rel|src\b/g
-  const matchTag = /\b(?<=[</])(DOCTYPE|a|link|abbr|acronym|address|applet|area|article|aside|audio|base|basefont|bdo|bgsound|blink|blockquote|body|br|button|canvas|caption|center|col|colgroup|command|comment|datalist|dd|del|details|dir|div|dl|dt|embed|fieldset|figure|b|big|i|small|tt|font|footer|form|frame|frameset|head|header|hgroup|h6|hr|html|isindex|iframe|ilayer|img|input|ins|keygen|keygen|label|layer|legend|li|link|map|mark|marquee|menu|meta|meter|multicol|nav|nobr|embed|noframes|noscript|object|ol|optgroup|option|output|p|param|cite|code|dfn|em|kbd|samp|strong|var|plaintext|pre|progress|q|rp|script|section|select|spacer|span|s|strike|style|sub|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|u|ul|video|wbr|nobr|xmp|link)\b/g
+  const matchTag = /\b(?<=[</])(DOCTYPE|a|abbr|acronym|address|applet|area|article|aside|audio|base|basefont|bdo|bgsound|blink|blockquote|body|br|button|canvas|caption|center|col|colgroup|command|comment|datalist|dd|del|details|dir|div|dl|dt|embed|fieldset|figure|b|big|i|small|tt|font|footer|form|frame|frameset|head|header|hgroup|h6|hr|html|isindex|iframe|ilayer|img|input|ins|keygen|keygen|label|layer|legend|li|link|map|mark|marquee|menu|meta|meter|multicol|nav|nobr|embed|noframes|noscript|object|ol|optgroup|option|output|p|param|cite|code|dfn|em|kbd|samp|strong|var|plaintext|pre|progress|q|rp|script|section|select|spacer|span|s|strike|style|sub|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|u|ul|video|wbr|nobr|xmp)\b/g
 
   function convertStrings () {
     example.innerHTML = ''
