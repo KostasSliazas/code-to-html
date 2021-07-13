0e9508773fcf1ddaea2874fcdc03ca2393684f84
Kostas <kostas.sliazas@gmail.com>
2021-07-13 04:44:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -95,44 +95,69 @@
   //   }
   // }
 
+  const mapObj = {
+    text: function (e) {
+      return `<i class="code__i--sc">${e}</i>`
+    },
+    simbols: function (e) {
+      const symbol = e === '<' ? '&lt;' : e === '>' ? '&gt;' : e === '=' ? '&equals;' : '&lt;&sol;'
+      return `<i class="code__i--re">${symbol}</i>`
+    },
+    tags: function (e) {
+      return `<i class="code__i--ta">${e}</i>`
+    }
+  }
+  const matchSimbols = /(?:[</]{2,2}|<|>|=)/g
+  const matchTexts = /\b(?<=\s)src|href|content|defer|rel|src\b/g
+  const matchTag = /\b(?<=[</])(DOCTYPE|a|link|abbr|acronym|address|applet|area|article|aside|audio|base|basefont|bdo|bgsound|blink|blockquote|body|br|button|canvas|caption|center|col|colgroup|command|comment|datalist|dd|del|details|dir|div|dl|dt|embed|fieldset|figure|b|big|i|small|tt|font|footer|form|frame|frameset|head|header|hgroup|h6|hr|html|isindex|iframe|ilayer|img|input|ins|keygen|keygen|label|layer|legend|li|link|map|mark|marquee|menu|meta|meter|multicol|nav|nobr|embed|noframes|noscript|object|ol|optgroup|option|output|p|param|cite|code|dfn|em|kbd|samp|strong|var|plaintext|pre|progress|q|rp|script|section|select|spacer|span|s|strike|style|sub|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|u|ul|video|wbr|nobr|xmp|link)\b/g
+
   function convertStrings () {
     example.innerHTML = ''
     const text = getValue(textInput)
     if (text) {
       const splitedText = text.split('\n')
       const fragment = document.createDocumentFragment()
-      const comment = /(\/\/.*)/g
       splitedText.filter(function (e) {
         return e.length > 0
       }).forEach(e => {
-        const mached = e.match(comment)
+        // create p tag elment
         const pTag = wraper('', 'p', 'code__p')
-        const splitedWord = e.split('')
-        const arr = splitedWord.map(e => {
-          if (e === '>') return e.replace('>', `<i class="code__i--el">${e}</i>`)
-          if (e === '<') return e.replace('<', `<i class="code__i--el">${e}</i>`)
-          if (e === '=') return e.replace('=', `<i class="code__i--el">${e}</i>`)
-          if (e === ',') return e.replace(',', `<i class="code__i--el">${e}</i>`)
-          if (e === ':') return e.replace(':', `<i class="code__i--el">${e}</i>`)
-          if (e === ';') return e.replace(';', `<i class="code__i--el">${e}</i>`)
-          return e
-        }).join('')
-          .replaceAll('href', '<i class="code__i--re"> href</i>')
-          .replaceAll('rel', '<i class="code__i--re"> rel</i>')
-          .replaceAll('src', '<i class="code__i--re"> src</i>')
-          .replaceAll('link', '<i class="code__i--sc">link</i>')
-          .replaceAll('"stylesheet"', '<i class="code__i--st">"stylesheet"</i>')
-          .replaceAll('script', '<i class="code__i--sc">script</i>')
-          .replaceAll(' defer ', '<i class="code__i--re"> defer </i>')
-          .replaceAll(comment, `<i class="code__i--co">${mached}</i>`)
-          .trim()
-        pTag.innerHTML = arr
+        // loop every line
+        // console.log(e)
 
+        // const str = e.replace(matchSimbols, matched => {
+        //   return mapObj.simbols(matched)
+        // })
+        const str = e.split(' ').map(e => {
+          let tagd
+          const machhed1 = e.match(matchTexts)
+          const matched2 = e.match(matchTag)
+          // const matched3 = e.match(matchSimbols)
+          e = e.replace(matchSimbols, matched => {
+            return mapObj.simbols(matched)
+          })
+          if (machhed1) {
+            tagd = e.replace(machhed1, matched => {
+              return mapObj.text(matched)
+            })
+          } else if (matched2) {
+            tagd = e.replace(matched2, matched => {
+              return mapObj.tags(matched)
+            })
+          } else tagd = e
+          return tagd
+        })
+        // const str1 = str.replace(matchTag, matched => {
+        //   return mapObj.tags(matched)
+        // })
+        // const str2 = str1.replace(matchTexts, matched => {
+        //   return mapObj.text(matched)
+        // })
+        pTag.innerHTML = str.join(' ')
         fragment.appendChild(pTag)
       })
-
       example.appendChild(fragment)
-      textOutput.value = example.outerHTML
+      textOutput.value = example.innerHTML
     } else {
       textOutput.value = 'no input'
     }
